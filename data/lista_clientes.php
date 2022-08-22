a6f149aba79e9a2f6099f3ddc1ec03d5fc3311b3
Jose Boch <jboch.99@gmail.com>
2022-08-22 17:47:43 -0600

    backend para registro de pedidos

---
+++ data/lista_clientes.php
@@ -0,0 +1,75 @@
+<?php
+require_once 'connection.php';
+
+//$cliente = $_GET["cliente"];
+
+$codigoRespuesta = 1;
+
+if ($mysqli !== null && $mysqli->connect_errno === 0) {
+    $stmt = "SELECT c.codigo," .
+        "c.primer_nombre as nombre," .
+        "c.iddepartamento," .
+        "c.id_municipio " .
+        "FROM clientes c " .
+        "WHERE c.estado = 1 " .
+       // "AND c.primer_nombre like '%$cliente%' ".
+        "ORDER BY c.primer_nombre;";
+
+    $result = $mysqli->query($stmt);
+
+    if ($result !== false) {
+        if ($result->num_rows > 0) {
+            $return_arr = array();
+            $indice = 0;
+            while ($row = $result->fetch_array()) {
+                $return_arr[$indice] = array(
+                    'codigo' => $row['codigo'],
+                    'nombre' => $row['nombre'],
+                    'iddepartamento' => $row['iddepartamento'],
+                    'idmunicipio' => $row['id_municipio']
+                );
+                $indice++;
+            }
+
+            echo json_encode($return_arr);
+            $result->close();
+        } else {
+            $codigoRespuesta = -3; //no hay registros
+            $result->close();
+        }
+    } else {
+        $codigoRespuesta = -2; //fallo en la consulta
+    }
+    $mysqli->close();
+} else {
+    $codigoRespuesta = -1; //fallo de conexión
+}
+
+if ($codigoRespuesta != 1) {
+    $mensajeRespuesta = "";
+    switch ($codigoRespuesta) {
+        case -1:{
+                $mensajeRespuesta = "FALLO DE CONEXION";
+                break;
+            }
+        case -2:{
+                $mensajeRespuesta = "FALLO DE CONSULTA";
+                break;
+            }
+        case -3:{
+                $mensajeRespuesta = "NO HAY REGISTROS";
+                break;
+            }
+    }
+
+    $return_arr = array();
+    $Indice = 0;
+    $return_arr[$Indice] = array(
+        'codigo' => $codigoRespuesta,
+        'nombre' => $mensajeRespuesta,
+        'iddepartamento' => 0,
+        'idmunicipio' => 0
+    );
+
+    echo json_encode($return_arr);
+}
---
+++ data/registro_pedido.php
@@ -0,0 +1,172 @@
+<?php
+
+$registroMain = $_POST["registro_principal"];
+$detalleRegistro = $_POST["detalle_registro"];
+
+//fecha registro
+date_default_timezone_set('America/Guatemala');
+$fechaHoraRegistro = date("Y-m-d H:i:s");
+
+$codigoRespuesta = 0;
+/**
+ * Se obtiene el registro principal del registro de pedido como objeto
+ */
+$jsonMain = json_decode($registroMain, false);
+
+$clienteID = $jsonMain->id_cliente;
+$departamentoID = 0;//$jsonMain->id_departamento;
+$municipioID = 0;//$jsonMain->id_municipio;
+$empleadoID = $jsonMain->id_empleado;
+$codigoCliente = '';//$jsonMain->codigo_cliente;
+$nombreCliente = ''; //$jsonMain->nombre_cliente;
+$observacionesRegistro = $jsonMain->observaciones;
+$estadoRegistro = 1;
+
+/**
+ * Se almacena en la base de datos
+ */
+require_once 'connection.php';
+
+if ($mysqli !== null) {   
+    //inicio de transacciones
+    $mysqli->autocommit(false);
+    //IDs
+    $query = "select " .
+        "if(max(id_solicitud) is null,1,max(id_solicitud) + 1) as id_solicitud, " .
+        "if(max(nosolicitud) is null,1,max(nosolicitud) + 1) as nosolicitud " .
+        "from vnt_solicitud_producto;";
+
+    $resultado = $mysqli->query($query);
+    $fila = $resultado->fetch_assoc();
+
+    $envioID = intval($fila["id_solicitud"]);
+    $solicitudID = intval($fila["nosolicitud"]);
+
+    try {
+        if (!$stmt = $mysqli->prepare(
+            "INSERT INTO vnt_solicitud_producto (" .
+            "id_solicitud," .
+            "nosolicitud," .
+            "id_departamento," .
+            "id_municipio,".
+            "id_empleado," .
+            "codigo_cliente," .
+            "nombre_cliente," .
+            "fecha_registro," .
+            "observaciones," .
+            "estado) " .
+            " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")) {
+            //echo "fallo No." . $mysqli->errno . " " . $mysqli->error;
+            $codigoRespuesta = -1;
+
+        } else
+        if (!$stmt->bind_param("iiiiissssi", $envioID, $solicitudID, $departamentoID,$municipioID, $empleadoID,
+            $codigoCliente, $nombreCliente, $fechaHoraRegistro, $observacionesRegistro, $estadoRegistro)) {
+            //echo "fallo la vinculacion: " . $mysqli->errno . " " . $mysqli->error;
+            $codigoRespuesta = -2;
+        } else if (
+            !$stmt->execute()) {
+            //echo "Fallo la ejecución " . $mysqli->errno . " " . $mysqli->error;
+            $codigoRespuesta = -3;
+        } else {
+
+            $filasAfectas = $mysqli->affected_rows;
+
+            if ($filasAfectas > 0) {
+                $codigoProducto = "";
+                $nombreProducto = "";
+                $cantidad = 0;
+                $tipoPrecio = "";
+                $precio = 0.00;
+                $subtotal = 0.00;
+                $observacionesProducto = "";
+                $estadoProducto = 1;
+                $empresaID = 0;
+
+                if (!$stmtd = $mysqli->prepare(
+                    "INSERT INTO vnt_detalle_solicitud_producto (" .
+                    "id_solicitud," .
+                    "codigo_producto," .
+                    "nombre_producto," .
+                    "cantidad," .
+                    "tipo_precio," .
+                    "precio," .
+                    "subtotal," .
+                    "observaciones," .
+                    "estado," .
+                    "id_empresa) " .
+                    " VALUES (?,?,?,?,?,?,?,?,?,?);")) {
+                    $codigoRespuesta = -4; //fallo al preparar la consulta de detalle
+
+                } else if (!$stmtd->bind_param("issisddsii", $envioID, $codigoProducto, $nombreProducto, $cantidad,
+                    $tipoPrecio, $precio, $subtotal, $observacionesProducto, $estadoProducto, $empresaID)) {
+                    $codigoRespuesta = -5; //no se pudieron vincular los parámetros a la consulta detalle
+                } else {
+                    /**
+                     *Se obtiene el detalle de los items como arreglo
+                     */
+                    $jsonDetalle = json_decode($detalleRegistro, true);
+
+                    $almacenado = true;
+
+                    foreach ($jsonDetalle as $item) {
+                        foreach ($item as $arr) {
+                            //$cadenaCompleta = $cadenaCompleta . $arr["nombre"];
+                            $codigoProducto = $arr["codigo_producto"];
+                            $nombreProducto = $arr["nombre_producto"];
+                            $cantidad = $arr["cantidad"];
+                            $tipoPrecio = $arr["tipoprecio"];
+                            $precio = $arr["precio"];
+                            $subtotal = $arr["subtotal"];
+                            $observacionesProducto = $arr["observaciones"];
+
+                            if (!$stmtd->execute()) {
+                                $codigoRespuesta = -6; //fallo al ejecutar la consulta detalle
+                                $almacenado = false;
+                                break 2;
+                            }
+                        }
+                    }
+
+                    if ($almacenado) {
+                        //se confirma la transacción
+                        $mysqli->commit();
+
+                        $codigoRespuesta = 1; //Operación correcta y terminada;
+                    } else {
+                        $mysqli->rollback();
+                    }
+                }
+
+            } else {
+                $mysqli->rollback();
+                if ($filasAfectas == 0) {
+                    $codigoRespuesta = -7; //no se hizo el insert principal
+                } else if ($filasAfectas == -1) {
+                    $codigoRespuesta = -8; //se produjo un error al insertar el registro principal
+                }
+            }
+        }
+
+    } catch (Exception $ex) {
+        $mysqli->rollback();
+        // echo $ex->getMessage();
+        $codigoRespuesta = -9; //Se produjo una excepción en la transacción
+    }
+
+    if ($stmtd) {
+        $stmtd->close();
+    }
+    if ($stmt) {
+        $stmt->close();
+    }
+
+    $mysqli->close();
+} else {
+
+    $codigoRespuesta = 0; //La conexión es null
+}
+
+//respuesta del WS hacia Android
+$jsonFinal = array("codigo" => $codigoRespuesta);
+echo json_encode($jsonFinal);
