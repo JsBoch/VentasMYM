2f52a9346acfbd44a5ae4f09aed83c9eae631428
Jose Gabriel <gabo.boch96@gmail.com>
2022-11-11 13:19:35 -0600

    aYa se arreglaron los estilos en vista para computadora y se hicieron otros cambios en el estilo

--- view/registro_pedido.view.php
+++ view/registro_pedido.view.php
@@ -32,27 +32,35 @@ if (!isset($_SESSION['estado']) || $_SESSION['estado'] != "conectado") {
                 </a>
             </div>
             <!-- cliente -->
-            <h2 class="main_title">Cliente</h2>
+            <h2 class="main_title">Datos Generales</h2>
             <div class="sub_container">
+                <div class="first_half">
+            <label for="departamento" class="subtitle_input">DEPARTAMENTO</label>
                 <select name="departamento" class="selectors" id="departamento" onchange="listaClientes()"></select>
                 <!--<select name="cliente" class="selectors" id="cliente"></select>-->
 
                 <!-- ASIGNAR FORMATO RESPONSIVE a cliente y ulclienteresult-->
+                <label for="cliente" class="subtitle_input">CLIENTE</label>
                 <input type="text" name="cliente" id="cliente" class="info_boxes" placeholder="Nombre de cliente" data-id="0" onblur="obtenerIdCliente()" autocomplete="off">
                 <ul id="ulclienteresult"  class="autocomplete_listClient"></ul>
                 <label for="sltPrioridad" class="subtitle_input">PRIORIDAD</label>
                 <select name="sltPrioridad" id="sltPrioridad" class="selector"></select>
+                </div>
+                <div class="second_half">
                 <label for="observaciones_producto" class="subtitle_input">OBSERVACIONES</label>
                 <textarea name="observaciones" class="comments" id="observaciones" cols="119" rows="5"></textarea>
                 <label for="transporte" class="subtitle_input">TRANSPORTE</label>
                 <input type="text" name="transporte" id="transporte" class="info_boxes" placeholder="Ingrese transporte">
+                </div>
             </div>
         </div>
         <!-- Pedido -->
         <div class="box_products">
-            <h2 class="main_title">Registro de Pedidos</h2>
+            <h2 class="main_title">Detalle de Productos</h2>
             <div class="second_sub_container">
+            <label for="codigo" class="subtitle_input">CODIGO</label>
                 <input type='text' name="codigo" class="info_boxes" id="codigo" placeholder="ingrese código" onchange="limpiarNombre()" autocomplete="off">
+                <label for="producto" class="subtitle_input">PRODUTO</label>
                 <input type="text" name="producto" class="info_boxes" id="producto" placeholder="ingreso nombre" size="100"  onchange="limpiarCodigo()" autocomplete="off">
 
                 <ul class="autocomplete_list" id="results"></ul>
@@ -74,9 +82,9 @@ if (!isset($_SESSION['estado']) || $_SESSION['estado'] != "conectado") {
                 <input type="text" name="subtotal" class="info_boxes" id="subtotal" placeholder="SUBTOTAL" autocomplete="off">
                 <label for="observaciones_producto" class="subtitle_input">OBSERVACIONES</label>
                 <div class="comentario">
-                <textarea name="observaciones_producto" class="comments" id="observaciones_producto" cols="30" rows="10"></textarea>
+                <textarea name="observaciones_producto" class="comments" id="observaciones_producto" cols="30" rows="5"></textarea>
                 </div>
-                <button class="add" onclick="cargarDetalle()" type="button">Agregar</button>
+                <button class="add" onclick="cargarDetalle()" type="button">Agregar al Pedido</button>
                 <button class="see" onclick="seeOrder('subContainerDates')" type="button">Ver Pedido</button>
             </div>
         </div>
@@ -86,9 +94,9 @@ if (!isset($_SESSION['estado']) || $_SESSION['estado'] != "conectado") {
         <select name="listado" id="listado" class="product_selector"></select>
         <button type="button" id="quitarRegistro" class="button_removeRegistry" name="quitarRegistro" onclick="QuitarItemDeLista()">Quitar Registro</button>
     </div>
-    <button class="add_more" onclick="backToOrders('subContainerDates')" type="button" id="shopping_cart">Agregar Más</button>
+    <button class="add_more" onclick="backToOrders('subContainerDates')" type="button" id="shopping_cart">Agregar otro producto</button>
     <a class="link_guardar" href="#subContainerDates">
-        <button class="save" type="button" id="send_order" onclick="GuardarRegistro()">Guardar</button>
+        <button class="save" type="button" id="send_order" onclick="GuardarRegistro()">Enviar Pedido</button>
     </a>
     <script src="../js/table.js"></script>
     <script src="../js/jquery-3.6.0.min.js"></script>
